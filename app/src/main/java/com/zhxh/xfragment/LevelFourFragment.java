1e3575390cafd4122343ccb4610736a7dde26793
zhxh <zhxhcoder@gmail.com>
2018-06-30 14:10:15 +0800

    Level test

--- app/src/main/java/com/zhxh/xfragment/LevelFourFragment.java
+++ app/src/main/java/com/zhxh/xfragment/LevelFourFragment.java
@@ -1,7 +1,36 @@
 package com.zhxh.xfragment;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by zhxh on 2018/6/30
  */
-public class LevelFourFragment {
+public class LevelFourFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public LevelFourFragment() {
+    }
+
+    public static LevelFourFragment newInstance(int sectionNumber) {
+        LevelFourFragment fragment = new LevelFourFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = rootView.findViewById(R.id.section_label);
+        textView.setText("LevelFourFragment " + getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    }
 }
--- app/src/main/java/com/zhxh/xfragment/MainActivity.java
+++ app/src/main/java/com/zhxh/xfragment/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return LevelFourFragment.newInstance(position + 1);
         }
 
         @Override
